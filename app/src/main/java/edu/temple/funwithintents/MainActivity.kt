eb866213447f98b8b720d469e3d93af4549e3797
johalloran01 <joshuamohalloran@gmail.com>
2023-11-07 12:00:08 -0500

    Created a textToShare value, created a sendIntent, utilized the shareSheet

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -12,10 +13,18 @@ class MainActivity : AppCompatActivity() {
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
+        val textToShare = editText.text.toString()
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, textToShare)
+                type = "text/plain"
+            }
 
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }
